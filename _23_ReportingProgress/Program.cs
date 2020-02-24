37c0e12e110127070e2065826ba3fa35fa231210
Bohdan <skripkabodya@gmail.com>
2020-02-24 17:13:39 +0200

    - IProgress test and usage

---
+++ _23_ReportingProgress/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _23_ReportingProgress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProgressReport.CallMethodAsync().GetAwaiter().GetResult();
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ _23_ReportingProgress/ProgressReport.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23_ReportingProgress
+{
+    class ProgressReport
+    {
+        private static bool isDone = false;
+
+        public static async Task MethodAsync(IProgress<int> progress = null)
+        {
+            int percentComplete = 0;
+
+            while (!isDone)
+            {
+                if (progress != null)
+                {
+                    progress.Report(percentComplete);
+                }
+
+                await Task.Delay(100);
+                percentComplete += 10;
+
+                if (percentComplete == 100)
+                {
+                    isDone = true;
+                }
+            }
+        }
+
+        public static async Task CallMethodAsync()
+        {
+            var progress = new Progress<int>();
+            progress.ProgressChanged += (sender, obj) =>
+            {
+                Console.WriteLine($"Complete: {obj}");
+            };
+
+            await MethodAsync(progress);
+        }
+    }
+}
