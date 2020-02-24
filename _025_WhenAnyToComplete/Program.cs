3d7419a88abf3dd6c2861dd78e1eb3e4a7fb04f8
Bohdan <skripkabodya@gmail.com>
2020-02-24 18:21:39 +0200

    - Task.WhenAny usage

---
+++ _025_WhenAnyToComplete/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _025_WhenAnyToComplete
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WhenAny.FirstRespondingUrlAsync("http://google.com", "http://google.com").GetAwaiter().GetResult();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ _025_WhenAnyToComplete/WhenAny.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace _025_WhenAnyToComplete
+{
+    class WhenAny
+    {
+        /// <summary>
+        /// WhenAny not throw original exception that mean to get errorMessage you need to
+        /// await firstCompletedTask or evalueate Result of any tasks
+        /// </summary>
+        /// <param name="urlA"></param>
+        /// <param name="urlB"></param>
+        /// <returns></returns>
+        public static async Task<int> FirstRespondingUrlAsync(string urlA, string urlB)
+        {
+            var httpClient = new HttpClient();
+
+            Task<byte[]> downloadTaskA = httpClient.GetByteArrayAsync(urlA);
+            Task<byte[]> downloadTaskB = httpClient.GetByteArrayAsync(urlB);
+
+            Task<byte[]> completedTask = await Task.WhenAny(downloadTaskA, downloadTaskB);
+            byte[] data = await completedTask;
+            return data.Length;
+        }
+    }
+}
