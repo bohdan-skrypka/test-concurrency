9125604e3727fa20871ab9aa82b4f761a2ef2383
Bohdan <skripkabodya@gmail.com>
2020-03-05 00:31:09 +0200

    - avoid continuations when use async keyword

---
+++ AvoidingContextForContinuations/AvoidContinueAwait.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _027_AvoidingContextForContinuations
+{
+    public class AvoidContinueAwait
+    {
+        public static async Task ResumeOnContextAsync()
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        }
+
+        public static async Task ResumeOnThreadPoolThread()
+        {
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            await Task.Delay(TimeSpan.FromSeconds(1)).ConfigureAwait(false);
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
---
+++ AvoidingContextForContinuations/Program.cs
@@ -0,0 +1,19 @@
+﻿using _027_AvoidingContextForContinuations;
+using System;
+using System.Threading;
+
+namespace AvoidingContextForContinuations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main" + Thread.CurrentThread.ManagedThreadId);
+
+            AvoidContinueAwait.ResumeOnContextAsync().GetAwaiter().GetResult();
+            AvoidContinueAwait.ResumeOnThreadPoolThread().GetAwaiter().GetResult();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
