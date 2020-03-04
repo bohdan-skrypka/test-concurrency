b2a833a851b33e890c58c74bf12a489f95547e2e
Bohdan <skripkabodya@gmail.com>
2020-03-05 00:42:56 +0200

    - handling exceptions

---
+++ _28_HandlingExceptions/ExceptionsAsync.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _028_HandlingExceptions
+{
+    public class ExceptionsAsync
+    {
+        public static async Task ThrowExceptionAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            throw new InvalidOperationException("Test");
+        }
+
+        public static async Task TestAsync()
+        {
+            try
+            {
+                await ThrowExceptionAsync();
+            }
+            catch (InvalidOperationException inEx)
+            {
+                Console.WriteLine(inEx.Message);
+            }
+        }
+
+        public static async Task TestAsyncExc()
+        {
+            var task = ThrowExceptionAsync();
+            try
+            {
+                await task;
+            }
+            catch (InvalidOperationException inEx)
+            {
+                Console.WriteLine(inEx.Message);
+            }
+        }
+    }
+}
---
+++ _28_HandlingExceptions/Program.cs
@@ -0,0 +1,15 @@
+﻿using _028_HandlingExceptions;
+using System;
+
+namespace _28_HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExceptionsAsync.TestAsync().GetAwaiter().GetResult();
+            ExceptionsAsync.TestAsyncExc().GetAwaiter().GetResult();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
